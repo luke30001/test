40ea226c988c4ab9e576201125b48b63c2e12cdb
luke30001 <41565281+luke30001@users.noreply.github.com>
2021-08-19 06:30:17 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def read(file):
+    try:
+        f = open(file, "r")
+        r=f.read()
+        f.close()
+        return r
+    except:
+        return ""
+def write(file,txt):
+    f = open(file, "w+")
+    f.write(txt)
+    f.close()
+def append(file,txt):
+    write(file,read(file)+txt)
